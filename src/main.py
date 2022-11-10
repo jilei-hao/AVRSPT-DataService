2f82235b51d3f460b91121ec5572e0fc189a3bf2
jilei-hao <jilei.cs@gmail.com>
2022-11-10 10:29:08 -0500

    solved dev CORS issue

--- src/main.py
+++ src/main.py
@@ -1,8 +1,20 @@
+import configparser
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-import configparser
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # read data dir from config
 config = configparser.ConfigParser()
